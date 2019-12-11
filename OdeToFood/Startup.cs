6265ff5bc69cac56c513a8ee9dbcf7086c9d0587
Daniil Vatahovych <daniil.vatahovych@iprospect.com>
2019-12-12 00:38:13 +0100

    EF was added

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,7 +6,9 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OdeToFood.Entities;
 using OdeToFood.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace OdeToFood
 {
@@ -32,7 +31,8 @@ namespace OdeToFood
             services.AddMvc(option=>option.EnableEndpointRouting = false);
             services.AddSingleton<IGreeter, Greeter>();
             services.AddSingleton(Configuration);
-            services.AddScoped<IRestaurantData, InMemoryRestaurantData>();
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
+            services.AddDbContext<OdeToFoodDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("OdeToFood")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
